69c177194a2d2fd0a31af300b5cd620b66f2c397
Syed Hunzala Ahmed Bukhari <k253089@nu.edu.pk>
2025-10-22 22:51:36 +0500

    Q7.c

--- Q7.c
+++ Q7.c
@@ -2,23 +2,21 @@
 
 int main() {
     int Arr[10];
-    
+    int freq[1000] = {0};
+
     printf("Enter 10 numbers: ");
-    
-    for(int i=0; i<=9; i++){
+    for(int i=0; i<10; i++){
         scanf("%d", &Arr[i]);
         
-        for(int j=0; j<i; j++){
-            if(Arr[i]==Arr[j]){
-                Arr[i]=-1;
-                break;
-            }
-        }
+        if(freq[Arr[i]] == 0)
+            freq[Arr[i]] = 1;
+        else
+            Arr[i] = -1;
     }
+
     printf("\nUpdated array (duplicates replaced with -1):\n");
-    for(int i=0; i<=9; i++){
+    for(int i=0; i<10; i++)
         printf("%d ", Arr[i]);
-    }
-    
+
     return 0;
 }
